91ad3edaa6a80e3d3dc0619dfba8491d0b68e8e2
Leonid Ushakov <gammbol2525@gmail.com>
2025-05-30 15:58:05 +0300

    fix url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'trposite-production.up.railway.app/api';
+const API_URL = 'htpps://trposite-production.up.railway.app/api';
 
 export const solveEquation = async (equation, variable = 'x') => {
   const response = await axios.post(`${API_URL}/solve`, {
